3384e26b634ddf14a5be151964507eeef1e95a03
Silvia Bormüller <sbormueller@gmail.com>
2015-07-09 15:26:51 +0200

    profile view edit
    profile view added

---
+++ RekelProjectApp/ProfileEditViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ProfileEditViewController.swift
+//  RekelProjectApp
+//
+//  Created by Silvia Bormüller on 09.07.15.
+//  Copyright (c) 2015 rekel. All rights reserved.
+//
+
+import UIKit
+
+class ProfileEditViewController: UIViewController {
+    
+    
+    // Container to store the current user
+    var currentUser : PFUser?
+    
+    
+    @IBOutlet weak var usersnameEditTextfield: UITextField!
+    
+    @IBOutlet weak var firstnameEditTextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Unwrap the current object object
+        if let object = currentUser {
+            usersnameEditTextfield.text = object["usersname"] as! String
+            firstnameEditTextfield.text = object["firstname"] as! String
+           
+        }
+        
+        
+        
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func userProfileSaveButton(sender: AnyObject) {
+        
+        if let object = currentUser {
+            
+            object["usersname"] = usersnameEditTextfield.text
+            object["firstname"] = firstnameEditTextfield.text
+           
+            
+            // Save the data back to the server in a background task
+          //object.saveEventually(nil)
+            
+          object.saveEventually()
+        object.ACL = PFACL(user: PFUser.currentUser()!)
+            
+            
+            //perform segue to tableview
+           
+            
+        }
+        
+         self.performSegueWithIdentifier("profileToTableView", sender: self)
+    }
+   
+
+}
