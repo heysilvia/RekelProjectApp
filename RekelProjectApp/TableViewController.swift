12edd7183c2eb8e656465c9585fcd511d335df32
Silvia Bormüller <sbormueller@gmail.com>
2015-07-09 17:32:44 +0200

    tableview Stuff

---
+++ RekelProjectApp/TableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TableViewController.swift
+//  RekelProjectApp
+//
+//  Created by Silvia Bormüller on 09.07.15.
+//  Copyright (c) 2015 rekel. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: PFQueryTableViewController {
+    
+    var currentObject : PFObject?
+    
+    // Initialise the PFQueryTable tableview
+    override init(style: UITableViewStyle, className: String!) {
+        super.init(style: style, className: className)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Configure the PFQueryTableView
+        self.parseClassName = "Items"
+        self.textKey = "type"
+        self.pullToRefreshEnabled = true
+        self.paginationEnabled = false
+    }
+    
+    
+    // Define the query that will provide the data for the table view
+    override func queryForTable() -> PFQuery {
+        var query = PFQuery(className: "Items")
+        query.orderByAscending("type")
+        return query
+    }
+    
+    //override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! PFTableViewCell!
+        if cell == nil {
+            cell = PFTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        }
+        
+        // Extract values from the PFObject to display in the table cell
+        if let itemType = object?["type"] as? String {
+            cell?.textLabel?.text = itemType
+        }
+        if let itemName = object?["name"] as? String {
+            cell?.detailTextLabel?.text = itemName
+        }
+        
+        return cell
+    }
+    
+}
